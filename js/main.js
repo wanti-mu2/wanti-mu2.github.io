09657d3eb35e547ad32958ffe665cb255b807a24
wanti-mu2 <94421188+wanti-mu2@users.noreply.github.com>
2022-07-16 20:43:22 +0900

    最終課題　アップロード

--- js/main.js
+++ js/main.js
@@ -1,61 +1,42 @@
-function quiz1(answer) {
-    let message = '';
+console.log('chart-test.js を読み込みました. ');
 
-    if (answer === 0) {
-        message = 'A. 15件　　　不正解！';
-    } else if (answer === 1) {
-        message = 'A. 25件　　　正解！！';
-    } else if (answer === 2) {
-        message = 'A. 35件　　　不正解！';
-    } else if (answer === 3) {
-        message = 'A. 45件　　　不正解！';
-    }
+const labels = [
+    '1994',
+    '1995',
+    '1996',
+    '1997',
+    '1998',
+    '1999',
+    '2000',
+    '2001',
+    '2002',
+    '2003',
+    '2004',
+];
 
-    console.log(message);
-    const p_quiz1 = document.querySelector('#quiz1');
-    p_quiz1.textContent = message;
-    return message;
-}
-
-function quiz2(answer) {
-    let message = '';
-
-    if (answer === 0) {
-        message = 'A. メキシコ合衆国　　　正解！！';
-    } else if (answer === 1) {
-        message = 'A. ハイチ共和国　　　不正解！';
-    } else if (answer === 2) {
-        message = 'A. エクアドル共和国　　　不正解！';
-    } else if (answer === 3) {
-        message = 'A. コロンビア共和国　　　不正解！';
-    }
-
-    console.log(message);
-    const p_quiz2 = document.querySelector('#quiz2');
-    p_quiz2.textContent = message;
-    return message;
-}
-
-function quiz3(answer) {
-    let message = '';
-
-    if (answer === 0) {
-        message = 'A. モン-サン-ミシェルとその湾　　　不正解！';
-    } else if (answer === 1) {
-        message = 'A. オークニー諸島の新石器時代遺跡中心地　　　不正解！';
-    } else if (answer === 2) {
-        message = 'A. ストーンヘンジ、エーヴベリーと関連する遺跡群　　　正解！！';
-    } else if (answer === 3) {
-        message = 'A. デルフィの古代遺跡　　不正解！';
-    }
-
-    console.log(message);
-    const p_quiz3 = document.querySelector('#quiz3');
-    p_quiz3.textContent = message;
-    return message;
-}
+const data = {
+    labels: labels,
+    datasets: [{
+        label: '琉球王国のグスフ及び関連遺産群',
+        backgroundColor: 'rgb(40, 70, 200)',
+        borderColor: 'rgb(40, 70, 200)',
+        data: [1841, 1852, 1771, 1887, 1974, 2096, 2117, 2035, 2362, 2513, 2455],
+    }, {
+        label: '原爆ドーム',
+        backgroundColor: 'rgb(500, 40, 95)',
+        borderColor: 'rgb(500, 40, 95)',
+        data: [1417, 1555, 1442, 1388, 1252, 1181, 1057, 1114, 1140, 1103, 1065],
+    }]
+};
 
 
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
 
-
-
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
